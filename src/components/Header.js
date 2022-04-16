e1a00d6a8247f3dd34cacb47f4c55b9135cb8e79
youngmin <ymkwon3@gnu.ac.kr>
2022-04-16 19:08:46 +0900

    헤더, 사이드 컨테이너 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,83 +9,74 @@ import { Button, Flex, Image, Text, Textarea } from "../elements";
 // components
 
 // react-icons
-import { AiFillHome } from "react-icons/ai";
-import { BiMessageDetail } from "react-icons/bi";
-import { IoMdAddCircleOutline } from "react-icons/io";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import {
+  RiHome4Fill,
+  RiHome4Line,
+  RiMessage2Fill,
+  RiMessage2Line,
+  RiAddBoxFill,
+  RiAddBoxLine,
+  RiCompass3Fill,
+  RiCompass3Line,
+} from "react-icons/ri";
 
-const Header = (props) => {
+import { history } from "../redux/configureStore";
+
+const Header = props => {
+  const logoClick = () => {
+    history.push("/");
+  };
+
   return (
-    <>
-      <Flex
-        height="60px"
-        position="fixed"
-        top="0"
-        zIndex="3"
-        borderBottom="1px solid rgb(219, 219, 219)"
-        bg="white"
-      >
-        <Flex padding="0 20px" maxWidth="975px" zIndex="10">
-          <Flex flex="1 0 127px" jc="flex-start">
-            <ToMain href="/">
-              <Text fontSize="32px">Instargram</Text>
-            </ToMain>
+    <Flex
+      height="60px"
+      position="fixed"
+      top="0"
+      zIndex="3"
+      borderBottom="1px solid rgb(219, 219, 219)"
+      bg="white"
+    >
+      <Flex padding="0 20px" maxWidth="975px" height="60px" zIndex="10">
+        <Flex flex="1 0 127px" jc="flex-start">
+          <Flex _onClick={logoClick} className="hoverEvent" width="105px">
+            <Image
+              shape="rectangle"
+              width="103px"
+              height="30px"
+              margin="5px 0 0 0"
+              src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+            ></Image>
           </Flex>
-          <SearchInput />
-          <HeadarIcons />
         </Flex>
+        <SearchDiv>
+          <SearchInput className="input" placeholder="검색"></SearchInput>
+          <Flex className="label" width="" height="36px" bg="#efefef">
+            <BiSearch className="icon" color="#8e8e8e" size="20"></BiSearch>
+            <Text margin="0 0 0 10px" fontSize="16px" fontWeight="300px">
+              검색
+            </Text>
+          </Flex>
+        </SearchDiv>
+
+        <HeadarIcons />
       </Flex>
-    </>
+    </Flex>
   );
 };
 
-Header.defaultProps = {};
-
-const ToMain = styled.a`
-  text-decoration: none;
-`;
-export default Header;
-
-const SearchInput = (props) => {
-  return (
-    <>
-      <Flex
-        width="268px"
-        height="36px"
-        minWidth="125px"
-        flex="0 1 auto"
-        bg="green"
-      ></Flex>
-    </>
-  );
-};
-
-SearchInput.defaultProps = {};
-
-export { SearchInput };
-
-const HeadarIcons = (props) => {
+const HeadarIcons = props => {
   return (
     <>
       <Flex flex="1 0 127px">
-        <Flex padding="0px 0px 0px 24px" whiteSpace="nowrap" jc="flex-end">
-          <a href="#">
-            <AiFillHome
-              style={{ fontSize: "30px", color: "black", marginLeft: "40px" }}
-            />
-          </a>
-          <a href="#">
-            <BiMessageDetail
-              style={{ fontSize: "30px", color: "black", marginLeft: "40px" }}
-            />
-          </a>
-          <a href="#">
-            <IoMdAddCircleOutline
-              style={{ fontSize: "30px", color: "black", margin: " 0 40px" }}
-            />
-          </a>
-          <a href="#">
-            <Image shape="circle" size={30} />
-          </a>
+        <Flex whiteSpace="nowrap" jc="flex-end" gap="22px">
+          <RiHome4Line color="#000" size="26" />
+          <RiMessage2Line color="#000" size="26" />
+          <RiAddBoxLine color="#000" size="26" />
+          <RiCompass3Line color="#000" size="26" />
+          <AiOutlineHeart color="#000" size="26"/>
+          <Image shape="circle" size={24} />
         </Flex>
       </Flex>
     </>
@@ -94,4 +85,37 @@ const HeadarIcons = (props) => {
 
 HeadarIcons.defaultProps = {};
 
-export { HeadarIcons };
+const SearchDiv = styled.div`
+  display: flex;
+  width: 268px;
+  height: 36px;
+  min-width: 125px;
+  flex: 0 1 auto;
+  background-color: #efefef;
+  padding: 0 16px;
+  border-radius: 8px;
+  position: relative;
+
+  .label {
+    position: absolute;
+  }
+
+  .input:focus + .label,
+  .input:not(:placeholder-shown) + .label {
+    display: none;
+  }
+`;
+
+const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  background-color: #efefef;
+  width: 100%;
+  height: 100%;
+  padding: 3px 0;
+  font-size: 16px;
+`;
+
+Header.defaultProps = {};
+
+export default Header;
